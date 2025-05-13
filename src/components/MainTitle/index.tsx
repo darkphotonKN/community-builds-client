ff83dac728cb5295f1cff20cd988ac85e654d545
Kranti <darkphoton20@gmail.com>
2025-05-14 01:46:52 +0800

    chore: forgot link

--- src/components/MainTitle/index.tsx
+++ src/components/MainTitle/index.tsx
@@ -1,12 +1,16 @@
+import Link from "next/link";
+
 type PageTitleProps = {
   title: string;
 };
 
 function MainTitle({ title }: PageTitleProps) {
   return (
-    <div className="min-w-[190px] flex gap-2 text-2xl font-medium mr-4">
-      {title}
-    </div>
+    <Link href="/">
+      <div className="min-w-[190px] flex gap-2 text-2xl font-medium mr-4">
+        {title}
+      </div>
+    </Link>
   );
 }
 
